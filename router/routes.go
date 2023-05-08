ebf9384dfbfb8eace4a272eefc4b80871e421df2
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2023-05-08 20:21:35 -0300

    create and configure router: add route test

---
+++ router/routes.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func configureRoutes(router *gin.Engine) {
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/home", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+		})
+		// v1.GET("/projects", handler.Projects)
+		// v1.GET("/projects/:id", handler.GetProject)
+		// v1.POST("/projects", handler.CreateProject)
+		// v1.PUT("/projects", handler.UpdateProject)
+		// v1.DELETE("/projects/:id", handler.DeleteProject)
+	}
+
+}
